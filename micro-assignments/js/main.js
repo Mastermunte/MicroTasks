9bbe8adb51973f88c7db95656ecfda0dc3125e15
Munteanu Alexandru <munte.alexandru@gmail.com>
2015-12-15 16:56:39 +0200

    Repo

--- micro-assignments/js/main.js
+++ micro-assignments/js/main.js
@@ -11,3 +11,28 @@ var listener = function() {
 };
 
 button.addEventListener("click", listener);
+
+
+
+
+var box = document.getElementById("box");
+
+var stars = document.getElementsByClassName("star");
+
+for ( var i = 0; i < stars.length; i++) {
+	var star = stars[i];
+	star.addEventListener("mouseover", function () {
+		var rating = this.getAttribute("data-value");
+		box.innerText = rating;
+		var length = parseInt(rating, 10);
+		for (var j = 0; j < length; j++) {
+			stars[j].style.color = "black";
+			};
+		});
+	star.addEventListener("mouseout", function () {
+		for (var j=0; j < stars.length; j++) {
+			stars[j].style.color = "gray";
+			box.innerText = "";
+			};
+		});
+	};
